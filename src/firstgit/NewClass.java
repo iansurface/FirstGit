687de40e532ded8feebbae1d132066695fa7036e
isurface <isurface@L11703042766.wctc.msft>
2017-01-26 21:04:08 -0600

    sout "You can do it, don't quit."

--- src/firstgit/NewClass.java
+++ src/firstgit/NewClass.java
@@ -9,14 +9,6 @@ package firstgit;
  *
  * @author isurface
  */
-public class FirstGit {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        System.out.println("You can do it, don't quit.");
-        int x = 1;
-    }
+public class NewClass {
     
 }
